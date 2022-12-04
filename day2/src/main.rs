a5575f7e29a5cf35536c577838369988dbf1a307
Timur Togyzbaev <timur.togyzbaev@instamart.ru>
2022-12-04 13:19:03 +0300

    day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,60 @@
 fn main() {
     let result = calculate_assumed_score("./input");
-    println!("{result}");
+    println!("1 {result}");
+    let result = calculate_strategy_score("./input");
+    println!("1 {result}");
+}
+
+enum FightOutcome {
+    Win,
+    Lose,
+    Draw,
+}
+fn calculate_strategy_score(file_path: &str) -> u64 {
+    let content = read_file(file_path);
+    content
+        .lines()
+        .map(|line| {
+            let mut chars = line.chars();
+            let opponent = match chars.nth(0).unwrap() {
+                'A' => Ok(Gesture::Rock),
+                'B' => Ok(Gesture::Paper),
+                'C' => Ok(Gesture::Scissors),
+                _ => Err("bad")
+            }.unwrap();
+            let needed_fight_outcome = match chars.nth(1).unwrap() {
+                'X' => Ok(FightOutcome::Lose),
+                'Y' => Ok(FightOutcome::Draw),
+                'Z' => Ok(FightOutcome::Win),
+                _ => Err("bad")
+            }.unwrap();
+            let me = match needed_fight_outcome {
+                FightOutcome::Win => {
+                    match opponent {
+                        Gesture::Rock => Gesture::Paper,
+                        Gesture::Scissors => Gesture::Rock,
+                        Gesture::Paper => Gesture::Scissors,
+                    }
+                },
+                FightOutcome::Draw => {
+                    match opponent {
+                        Gesture::Rock => Gesture::Rock,
+                        Gesture::Scissors => Gesture::Scissors,
+                        Gesture::Paper => Gesture::Paper,
+                    }
+                },
+                FightOutcome::Lose => {
+                    match opponent {
+                        Gesture::Rock => Gesture::Scissors,
+                        Gesture::Scissors => Gesture::Paper,
+                        Gesture::Paper => Gesture::Rock,
+                    }
+                },
+            };
+            calculate_fight_score(FightRound(opponent, me))
+        })
+        .sum()
+
 }
 
 fn calculate_assumed_score(file_path: &str) -> u64 {
